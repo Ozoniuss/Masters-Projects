5316d82c19098a741d25c0b4257a6efb7760fe9d
ozoniuss <alexbledea_2000@yahoo.com>
2022-11-23 00:24:17 +0200

    feat(TPJAD): gradle tutorials

---
+++ semester-1/Java-Technologies-and-Models-for-Distributed-Systems/tutorials/04_gradle/04/src/main/java/Hw.java
@@ -0,0 +1,9 @@
+import org.apache.log4j.*;
+public class Hw { 
+    static Logger log = Logger.getLogger(Hw.class);
+    public static void main(String[] args) {
+        log.info("1");
+        System.out.println("Hello World!");
+        log.info("2");
+    }
+}
