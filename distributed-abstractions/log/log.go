40f6efec3b71b4a4997f00d25d526f8b31002166
Ozoniuss <qscxzsewqazxcde@gmail.com>
2023-04-13 16:00:45 +0300

    feat(da): make log simpler
    It's such a complex project a global variable actually
    sounds like a good idea

--- distributed-abstractions/log/log.go
+++ distributed-abstractions/log/log.go
@@ -5,71 +5,14 @@ import (
 	"os"
 )
 
-type loglevel int
+// logger simply overwrites the default logger since microseconds matter in this
+// case.
+var logger = log.New(os.Stdout, "", log.Ltime|log.Lmicroseconds)
 
-const (
-	INFO  loglevel = 0
-	DEBUG loglevel = 1
-	TRACE loglevel = 2
-)
-
-type Logger struct {
-	level loglevel
-	info  *log.Logger
-	debug *log.Logger
-	trace *log.Logger
-	errlg *log.Logger
+func Println(v ...any) {
+	logger.Println(v...)
 }
 
-func InitLog(level loglevel) Logger {
-	info := log.New(os.Stdout, "INF ", log.Lmicroseconds)
-	debug := log.New(os.Stdout, "DBG ", log.Lmicroseconds)
-	trace := log.New(os.Stdout, "TRC ", log.Lmicroseconds)
-	errlg := log.New(os.Stdout, "ERR ", log.Lmicroseconds)
-
-	return Logger{
-		level: level,
-		info:  info,
-		debug: debug,
-		trace: trace,
-		errlg: errlg,
-	}
-}
-
-func (l *Logger) Info(v ...any) {
-	if l.level >= INFO {
-		l.info.Println(v...)
-	}
-}
-func (l *Logger) Infof(format string, v ...any) {
-	if l.level >= INFO {
-		l.info.Printf(format, v...)
-	}
-}
-func (l *Logger) Debug(v ...any) {
-	if l.level >= DEBUG {
-		l.debug.Println(v...)
-	}
-}
-func (l *Logger) Debugf(format string, v ...any) {
-	if l.level >= DEBUG {
-		l.debug.Printf(format, v...)
-	}
-}
-func (l *Logger) Trace(v ...any) {
-	if l.level >= TRACE {
-		l.trace.Println(v...)
-	}
-}
-func (l *Logger) Tracef(format string, v ...any) {
-	if l.level >= TRACE {
-		l.trace.Printf(format, v...)
-	}
-}
-
-func (l *Logger) Error(v ...any) {
-	l.errlg.Println(v...)
-}
-func (l *Logger) Errorf(format string, v ...any) {
-	l.errlg.Printf(format, v...)
+func Printf(format string, v ...any) {
+	logger.Printf(format, v...)
 }
